b62658070c887c4cec6f3ea9ee92d36b42946c35
mbjik <m.b.jikre@gmail.com>
2026-02-05 12:12:59 +0530

    New chapter

--- Python Basics/6. Strings.py
+++ Python Basics/6. Strings.py
@@ -21,3 +21,11 @@ print(long_string)
 
 combined = username + ' & ' + password
 print(combined)
+
+a = 'henlo'
+b = str('5')
+print(a + ' ' + b)
+print(type(int(str(5))))
+
+weather = "It\'s \t\"kind of\"\t sunny \nHave a good day"
+print(weather)
---
+++ Python Basics/7. Formatted Strings.py
@@ -0,0 +1,5 @@
+name = input()
+age = int(input())
+
+print('Hi ' + name + '. You are ' + str(age) + ' years old')
+print(f'Hi {name}. You are {age} years old')
