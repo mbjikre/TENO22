c37fac195d23bd84e37c288a4236a3444e343792
mbjik <m.b.jikre@gmail.com>
2026-02-05 22:14:05 +0530

    New chapter

--- Python Basics/12. Dictionary.py
+++ Python Basics/12. Dictionary.py
@@ -5,3 +5,26 @@ dictionary = {
 }
 
 print(type(dictionary))
+
+dictionary = {
+    123 : [1, 2, 3],
+    True : 'heellloooo',
+    23 : False
+}
+
+print(dictionary[True])
+
+user = {
+    'name' : 'Jojo',
+    'gen' : 'male',
+    'age' : 22
+}
+
+print(user.get('age', 55))
+print(22 in user.values())
+
+user1 = dict(abc = '123')
+print(type(user1))
+
+tup = (1,2,3,4,5)
+print(5 in tup)
