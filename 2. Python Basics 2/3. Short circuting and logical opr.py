2ae28d28f10d060aa5138d53fb71996005de0748
mbjik <m.b.jikre@gmail.com>
2026-02-07 15:28:46 +0530

    New chapter

--- 2. Python Basics 2/3. Short circuting and logical opr.py
+++ 2. Python Basics 2/3. Short circuting and logical opr.py
@@ -1,3 +1,10 @@
+# <
+# <=
+# >
+# >=
+# ==
+# !=
+
 a = True
 b = True
 
@@ -6,3 +13,29 @@ if a and b:
 
 if a or b:
     print('condition don\'t match')
+
+a = 0
+b = 0
+print(a == b)
+
+
+is_m = False
+is_e = False
+
+if is_m and is_e:
+    print('You are a master magician')
+elif is_m and not is_e:
+    print('Atleast you\'re getting there')
+elif not is_m:
+    print('You need magic powers')
+else:
+    print('you sir are a total loser')
+
+
+print(bool(''))
+print(True == 1)
+print('1' == 1)
+print([1] == 1)
+
+ind = (1,2,3,4,5)
+print(1 == ind[0])
