7a616441f9cda1341cb224325ba8794ee180dbd9
mbjik <m.b.jikre@gmail.com>
2026-02-07 16:15:47 +0530

    New chapter

---
+++ 2. Python Basics 2/For loop.py
@@ -0,0 +1,28 @@
+fst = input('Please enter a choice:\n1. Multiplication\n2. Division\n3. Addition\n4. Substraction :- ')
+for i in fst:
+    if i == '1':
+        print('\nEnter TWO numbers')
+        c = int(input())
+        d = int(input())
+        print(f'The result is : {c*d}')
+    
+    elif i == '2':
+        print('\nEnter TWO numbers')
+        c = int(input())
+        d = int(input())
+        print(f'The result is : {c*d}')
+    
+    elif i == '3':
+        print('\nEnter TWO numbers')
+        c = int(input())
+        d = int(input())
+        print(f'The result is : {c*d}')
+    
+    elif i == '4':
+        print('\nEnter TWO numbers')
+        c = int(input())
+        d = int(input())
+        print(f'The result is : {c*d}')
+    
+    else:
+        print('Wrong input')
