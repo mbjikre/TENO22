8a6d89ed731ca18d4cee8a506067103e332dc47c
mbjik <m.b.jikre@gmail.com>
2026-02-08 22:19:55 +0530

    New chapter

--- 2. Python Basics 2/6. While.py
+++ 2. Python Basics 2/6. While.py
@@ -1,3 +1,25 @@
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+]
+
+for i in picture:
+    for p in i:
+        if p == 1:
+            print('^', end='')
+        else:
+            print(' ', end='')
+    print('')
+
+while True:
+    l = input()
+    if l == 'exit':
+        break
 i = 0
 while i < 50:
     i += 1
@@ -9,3 +31,9 @@ while ini <= 5:
         a = int(input())
         b = int(input())
         print(f'Result is {a*b}')
+        break
+
+f=0
+while f < len([1,2,3,4,5]):
+   print('hi')
+   f += 1
