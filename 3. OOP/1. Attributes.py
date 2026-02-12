bd5acb628165333a6a634c3e1a71eae4d28f21ec
mbjik <m.b.jikre@gmail.com>
2026-02-12 09:38:28 +0530

    1. Attributes.py

---
+++ 3. OOP/1. Attributes.py
@@ -0,0 +1,16 @@
+class user:
+    def __init__(self, name, age, work):
+        self.name = input('Enter username: ')
+        self.age = input('Enter age: ')
+        self.work = input('Enter work: ')
+    
+    def work(self):
+        pass
+
+user1 = user(1, 2, 3)
+user2 = user(1, 2, 3)
+
+
+print(user1.name, user1.age, user1.work)
+print(user2.name, user2.age, user2.work)
+
